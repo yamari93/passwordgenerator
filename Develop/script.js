55a03bb2b8534093e0ddd65fb5d67c4ff511eb87
yamari <yamarigonzalez25@gmail.com>
2022-11-13 16:37:55 -0500

    psuedo coded & added number array & random generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,34 @@
+//password generator will create a random password between 8-50 characters which may include;numbers, lower case letters, upper case letters, and special characters.
+//global variables
+
+//database:numbers, lower case letters, upper case letters, and special characters
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; //number array
+//lower case letter array
+//upper case letter array
+//special characters array
+let passwordLength = 5;
+let ranNumber = randomNumber(10);
+console.log(ranNumber);
+let password = number[ranNumber];
 // Assignment code here
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// This function will generate the password & return it to the correct password
+function generatePassword() {
+  //build up a password using random numbers as long as the length
+  return password;
+}
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max); //generates random
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
